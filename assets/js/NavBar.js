ca1217866cf99e66d8bcc3e8cae53ed0b2f975e0
Raphael <raphalizier2@gmail.com>
2026-09-18 02:14:56 +0200

    second commit

--- assets/js/NavBar.js
+++ assets/js/NavBar.js
@@ -1,60 +1,249 @@
-function CloseContact(){
-    const contact = document.getElementById('wdw-contact');
-    const project = document.getElementById('wdw-project');
-    const bouton = document.getElementById('close-btn');
+const project = document.getElementById('wdw-project');
+const contact = document.getElementById('wdw-contact');
+const stacks = document.getElementById('wdw-stacks');
+const profil =document.getElementById('wdw-profil');
 
-    bouton.addEventListener('click', function(){
-        contact.style.display = 'none';
-        project.style.filter = 'brightness(100%)';
-    });
-}
+function OpenAndCloseContact(){
+    const openbtn = document.getElementById('btn-contact');
+    const closebtn = document.getElementById('close-btn');
+    const span = document.querySelector('.navbarcontainer__navbar__btn-nav__contact-span');
 
-function OpenContact(){
-    const contact = document.getElementById('wdw-contact');
-    const project = document.getElementById('wdw-project');
-    const navbtn = document.getElementById('btn-contact');
-
-    navbtn.addEventListener('click', function(){
+    openbtn.addEventListener('click', function(){
         contact.style.display = 'flex';
-        contact.style.zIndex = '1';
-        project.style.zIndex = '0';
         contact.style.filter = 'brightness(100%)';
         project.style.filter = 'brightness(70%)';
+        stacks.style.filter = 'brightness(70%)';
+        profil.style.filter = 'brightness(70%)';
+        span.style.display = 'block';
+        contact.style.zIndex='2';
+        if (project.style.zIndex=="2") {
+            project.style.zIndex="1";
+            profil.style.zIndex="0";
+        }else if(profil.style.zIndex=="2" && project.style.zIndex!=="-1"){
+            project.style.zIndex="0";
+            profil.style.zIndex="1";
+        }
+
+        if (stacks.style.zIndex=="2") {
+            stacks.style.zIndex="1";
+            profil.style.zIndex="0";
+        }else if(profil.style.zIndex=="2"){
+            stacks.style.zIndex="0";
+            profil.style.zIndex="1";
+        }
+    });
+
+    closebtn.addEventListener('click', function(){
+        contact.style.display = 'none';
+        span.style.display = 'none';
+        if (project.style.zIndex=="1") {
+            project.style.zIndex="2";
+            project.style.filter = 'brightness(100%)';
+            profil.style.zIndex="1";
+        }else{
+            profil.style.zIndex="2";
+            profil.style.filter = 'brightness(100%)';
+            project.style.zIndex="1";
+        }
+
+        if (stacks.style.zIndex=="1") {
+            stacks.style.zIndex="2";
+            stacks.style.filter = 'brightness(100%)';
+            profil.style.zIndex="1";
+        }else{
+            profil.style.zIndex="2";
+            profil.style.filter = 'brightness(100%)';
+            stacks.style.zIndex="1";
+        }
     });
 }
 
-function OpenProject() {
-    const project = document.getElementById('wdw-project');
-    const navbtn = document.getElementById('btn-project');
-    const contact = document.getElementById('wdw-contact');
+function OpenAndCloseProject() {
+    const openbtn = document.getElementById('btn-project');
+    const secondopenbtn = document.getElementById('s-project-line');
+    const closebtn = document.getElementById('project-close-btn');
+    const span = document.querySelector('.navbarcontainer__navbar__btn-nav__project-span');
+    const spanStacks = document.querySelector('.navbarcontainer__navbar__btn-nav__stacks-span');
 
-    navbtn.addEventListener('click', function(){
+
+    openbtn.addEventListener('click', function(){
         project.style.display = 'flex';
-        project.style.zIndex = '1';
-        contact.style.zIndex = '0';
+        stacks.style.display = 'none';
+        spanStacks.style.display = 'none';
         project.style.filter = 'brightness(100%)';
         contact.style.filter = 'brightness(70%)';
+        profil.style.filter = 'brightness(70%)';
+        span.style.display = 'block';
+        project.style.zIndex='2';
+        if (contact.style.zIndex=="2") {
+            contact.style.zIndex="1";
+            profil.style.zIndex="0";
+        }else if (profil.style.zIndex=="2"){
+            contact.style.zIndex="0";
+            profil.style.zIndex="1";
+        }
+    });
+
+    secondopenbtn.addEventListener('click', function(){
+        project.style.display = 'flex';
+        stacks.style.display = 'none';
+        spanStacks.style.display = 'none';
+        project.style.filter = 'brightness(100%)';
+        contact.style.filter = 'brightness(70%)';
+        profil.style.filter = 'brightness(70%)';
+        span.style.display = 'block';
+        project.style.zIndex='2';
+        if (contact.style.zIndex=="2") {
+            contact.style.zIndex="1";
+            profil.style.zIndex="0";
+        }else if (profil.style.zIndex=="2"){
+            contact.style.zIndex="0";
+            profil.style.zIndex="1";
+        }
+    });
+
+    closebtn.addEventListener('click', function(){
+        project.style.display='none';
+        span.style.display = 'none';
+        if (contact.style.zIndex=="1") {
+            contact.style.zIndex="2";
+            contact.style.filter='brightness(100%)';
+            profil.style.zIndex="1";
+        }else{
+            profil.style.zIndex="2";
+            profil.style.filter = 'brightness(100%)';
+            contact.style.zIndex="1";
+        }
+    });
+    
+}
+
+function OpenAndCloseStacks() {
+    const openbtn = document.getElementById('btn-stacks');
+    const secondopenbtn = document.getElementById('p-stacks-line');
+    const span = document.querySelector('.navbarcontainer__navbar__btn-nav__stacks-span');
+    const spanProject = document.querySelector('.navbarcontainer__navbar__btn-nav__project-span');
+    const closebtn = document.getElementById('stacks-close-btn');
+
+    openbtn.addEventListener('click', function(){
+        stacks.style.display = 'flex';
+        span.style.display = 'block';
+        project.style.display = 'none';
+        stacks.style.filter = 'brightness(100%)';
+        contact.style.filter = 'brightness(70%)';
+        profil.style.filter = 'brightness(70%)';
+        spanProject.style.display = 'none';
+        stacks.style.zIndex = '2';
+        project.style.zIndex = '-1';
+        if (contact.style.zIndex=="2") {
+            contact.style.zIndex="1";
+            profil.style.zIndex="0";
+        }else if (profil.style.zIndex=="2"){
+            contact.style.zIndex="0";
+            profil.style.zIndex="1";
+        }
+    })
+
+    secondopenbtn.addEventListener('click', function(){
+        stacks.style.display = 'flex';
+        span.style.display = 'block';
+        project.style.display = 'none';
+        stacks.style.filter = 'brightness(100%)';
+        contact.style.filter = 'brightness(70%)';
+        profil.style.filter = 'brightness(70%)';
+        spanProject.style.display = 'none';
+        stacks.style.zIndex = '2';
+        project.style.zIndex = '-1';
+        if (contact.style.zIndex=="2") {
+            contact.style.zIndex="1";
+            profil.style.zIndex="0";
+        }else if (profil.style.zIndex=="2"){
+            contact.style.zIndex="0";
+            profil.style.zIndex="1";
+        }
+    })
+
+    closebtn.addEventListener('click', function(){
+        stacks.style.display='none';
+        span.style.display = 'none';
+        if (contact.style.zIndex=="1") {
+            contact.style.zIndex="2";
+            contact.style.filter='brightness(100%)';
+            profil.style.zIndex="1";
+        }else{
+            profil.style.zIndex="2";
+            profil.style.filter = 'brightness(100%)';
+            contact.style.zIndex="1";
+        }
     })
 }
 
-function CloseProject() {
-    const project = document.getElementById('wdw-project');
-    const contact = document.getElementById('wdw-contact');
-    const bouton = document.getElementById('project-close-btn');
+function OpenAndCloseProfil() {
+    const openbtn = document.getElementById('btn-profil');
+    const span = document.querySelector('.navbarcontainer__navbar__btn-nav__profil-span');
+    const closebtn = document.getElementById('profil-close-btn');
+
+    openbtn.addEventListener('click', function(){
+        profil.style.display = 'flex';
+        profil.style.filter = 'brightness(100%)';
+        project.style.filter = 'brightness(70%)';
+        stacks.style.filter = 'brightness(70%)';
+        contact.style.filter = 'brightness(70%)';
+        span.style.display = 'block';
+        profil.style.zIndex = '2';
+        if (project.style.zIndex=="2") {
+            project.style.zIndex="1";
+            contact.style.zIndex="0";
+        }else if(contact.style.zIndex=="2" && project.style.zIndex!=="-1"){
+            project.style.zIndex="0";
+            contact.style.zIndex="1";
+        };
+
+        if (stacks.style.zIndex=="2") {
+            stacks.style.zIndex="1";
+            contact.style.zIndex="0";
+        }else if(contact.style.zIndex=="2"){
+            stacks.style.zIndex="0";
+            contact.style.zIndex="1";
+        };
+    });
+
+    closebtn.addEventListener('click', function(){
+        profil.style.display='none';
+        span.style.display='none';
+        if (project.style.zIndex=="1") {
+            project.style.zIndex="2";
+            project.style.filter = 'brightness(100%)';
+            contact.style.zIndex="1";
+        }else{
+            contact.style.zIndex="2";
+            contact.style.filter='brightness(100%)';
+            project.style.zIndex="1";
+        }
+
+        if (stacks.style.zIndex=="1") {
+            stacks.style.zIndex="2";
+            stacks.style.filter = 'brightness(100%)';
+            contact.style.zIndex="1";
+        }else{
+            contact.style.zIndex="2";
+            contact.style.filter='brightness(100%)';
+            stacks.style.zIndex="1";
+        }
+    });
+}
+
+function CloseRecoPage() {
+    const recopage = document.getElementById('reco-container');
+    const bouton = document.getElementById('close-reco-btn');
 
     bouton.addEventListener('click', function(){
-        project.style.display='none';
-        contact.style.filter = 'brightness(100%)';
-    })
+        recopage.style.display='none';
+    });
 }
 
-/*
-Page projet et stacks superposées et quand l'une est fermée, l'autre se ferme aussi.
-Page projet et page stacks accessibles depuis la nav mais aussi depuis la nav de l'explorateur de fichier (pas encore faite). 
-Pour gerer filtre noir quand superposition des pages, jouer avec z-index et if.
-*/
-
-CloseContact();
-OpenContact();
-OpenProject();
-CloseProject();
+OpenAndCloseContact();
+OpenAndCloseProject();
+OpenAndCloseStacks();
+OpenAndCloseProfil();
+CloseRecoPage();
